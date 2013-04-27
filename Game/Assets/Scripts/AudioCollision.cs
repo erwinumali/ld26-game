0cddfcdfc6f59a17625684ab2cb5839740f1ffd8
Nigel Rodriguez <anr.rod@gmail.com>
2013-04-27 21:09:03 +0800

    added soundcollision

---
+++ Game/Assets/Scripts/AudioCollision.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class AudioCollision : MonoBehaviour {
+	private RaycastHit hit;
+	private GameObject player;
+	private Transform c_transform;
+	private AudioSource audio; 
+	private float origVol;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+		c_transform = this.transform;
+		audio =  this.GetComponent<AudioSource>();
+		origVol = audio.maxDistance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 origin = c_transform.position;
+		Vector3 dest = player.transform.position;
+		float pDist = Vector3.Distance(origin, dest);
+		Debug.DrawRay(origin, (dest - origin), Color.green);
+		Physics.Raycast(origin, Vector3.Normalize(dest - origin), out hit, pDist);
+		if (hit.transform && hit.transform.gameObject != player) {
+			Debug.DrawRay(origin, (hit.point - origin), Color.red);
+
+			float cDist = Vector3.Distance(origin, hit.point);
+
+			//audio.maxDistance = (cDist / pDist) * origVol;
+			audio.maxDistance = cDist;
+		} else {
+			audio.maxDistance = origVol;
+		}
+	}
+}
