08c4ead0387cfabfb15950498a5ad6d6332c4f6f
Erwin Umali <kazukishida@gmail.com>
2013-04-27 14:44:27 +0800

    add initial test scene

---
+++ Game/Assets/Init.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class Init : MonoBehaviour {
+	
+	public GameObject light;
+	// Use this for initialization
+	void Start () {
+		light.active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
