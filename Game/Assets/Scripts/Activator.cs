51ad1fbc038132710f8fbca918f7202855c62dcd
Nigel Rodriguez <anr.rod@gmail.com>
2013-04-27 18:56:34 +0800

    triggers, light fx

---
+++ Game/Assets/Scripts/Activator.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using System.Collections;
+
+public class Activator : MonoBehaviour {
+	public virtual void Activate() {}
+	public virtual void Deactivate() {}
+}
---
+++ Game/Assets/Scripts/FlashActivator.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class FlashActivator : Activator {
+	public bool isActive;
+	private float limit = 150.0f;
+	private float newRange;
+	private Light light;
+
+	public override void Activate() {
+		isActive = true;
+	}
+
+	public override void Deactivate() {
+		isActive = false;
+	}
+
+	void Start () {
+		light = this.transform.GetComponentInChildren<Light>();
+		newRange = light.range;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (isActive) {
+			if (newRange < limit) {
+				newRange += 100.0f * Time.deltaTime;
+				light.range = newRange;
+			}
+		}
+	}
+}
