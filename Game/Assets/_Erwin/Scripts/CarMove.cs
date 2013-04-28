a45cb8235cdd2d803e686f6e6ecd0eff6dfa6bfe
Nigel Rodriguez <anr.rod@gmail.com>
2013-04-28 18:24:48 +0800

    improved carmove function, add 1 more car

--- Game/Assets/_Erwin/Scripts/CarMove.cs
+++ Game/Assets/_Erwin/Scripts/CarMove.cs
@@ -6,7 +6,10 @@ public class CarMove : MonoBehaviour {
 	public GameObject startPoint;
 	public GameObject endPoint;
 	
-	public float rate = 2.0f;
+	public float rate = 10.0f;
+	public float delayRange = 50.0f;
+	public float delay;
+	public float counter;
 	
 	private Transform car;
 	
@@ -14,14 +17,23 @@ public class CarMove : MonoBehaviour {
 	void Start () {
 		car = transform;
 		car.position = startPoint.transform.position;
+		counter = 0;
+		delay = Random.value * delayRange;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(car.position != endPoint.transform.position){
-			car.position = Vector3.MoveTowards(car.position, endPoint.transform.position, rate);
+			car.position = Vector3.MoveTowards(car.position, endPoint.transform.position, rate * Time.deltaTime);
 		} else {
-			car.position = startPoint.transform.position;
+			if (counter < delay) {
+				counter += 5.0f * Time.deltaTime;
+			} else {
+				car.position = startPoint.transform.position;
+				delay = Random.value * delayRange;
+				counter = 0;
+			}
 		}
 	}
 }
