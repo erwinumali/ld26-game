818db9020ed6c09c0615681c38c6eda690f55462
leafarz <rafaelformes@yahoo.com>
2013-04-28 19:21:42 +0800

    added footstep scene

--- Game/Assets/_Raf/Scripts/Footsteps.cs
+++ Game/Assets/_Raf/Scripts/Footsteps.cs
@@ -3,9 +3,26 @@ using System.Collections;
 
 public class Footsteps : MonoBehaviour {
 	
-
+	private AudioSource _stepSoundPlayer;
+    public AudioClip _randomStepSounds;
 	// Update is called once per frame
-	void Update () {
-
-	}
+ 
+    void Start(){
+       _stepSoundPlayer=gameObject.GetComponent<AudioSource>();  
+    }
+ 
+    void Update(){
+		//sample pa lang to mga sir
+		if(Input.GetKey(KeyCode.W)){
+			if(!_stepSoundPlayer.isPlaying) _stepSoundPlayer.Play();
+		}
+		//uncomment if gusto nyo ng stop / pause
+		/*
+		else{
+			//_stepSoundPlayer.Pause();
+			_stepSoundPlayer.Stop();
+		}
+		*/
+    }
+	
 }
