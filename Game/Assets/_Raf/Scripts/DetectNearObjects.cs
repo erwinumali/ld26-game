1774c149bfbe80ff1a2333d24f2df43b941cbe5a
Raf <r.formes@klab.com>
2013-05-21 08:13:21 +0800

    testScene4

--- Game/Assets/_Raf/Scripts/DetectNearObjects.cs
+++ Game/Assets/_Raf/Scripts/DetectNearObjects.cs
@@ -15,25 +15,49 @@ public class DetectNearObjects : MonoBehaviour {
 	void Update () {
 		Collider[] colliders = Physics.OverlapSphere(transform.position,vision*1.25f,mask);
 		foreach(Collider col in colliders){
-			distance=Vector3.Distance(c_transform.position, col.ClosestPointOnBounds(c_transform.position));
-			LineRenderer lineRenderer=col.GetComponent<LineRenderer>();
 			WireFrame wireFrame=col.GetComponent<WireFrame>();
-			if(distance<=vision){
-				wireFrame.enabled=true;
-				lineRenderer.enabled=true;
-				if(wireFrame.enabled && lineRenderer.enabled){
-					wireFrame.SetWireFrame(distance);
-				}
+			// get least distance still gets from non has parent colliders
+			if(wireFrame.HasParent()){
+				distance=GetLeastDistance(colliders);
+				if(distance<=vision) wireFrame.GetParent().SetWireFrames(distance);
+				else wireFrame.GetParent().SetWireFrames(0);
 			}else{
-				if(wireFrame.enabled && lineRenderer.enabled) wireFrame.SetWireFrame(0);
-				wireFrame.enabled=false;
-				lineRenderer.enabled=false;
+				distance=Vector3.Distance(c_transform.position, col.ClosestPointOnBounds(c_transform.position));
+				LineRenderer lineRenderer=col.GetComponent<LineRenderer>();
+				if(distance<=vision){
+					wireFrame.enabled=true;
+					lineRenderer.enabled=true;
+					wireFrame.SetWireFrame(distance);
+				}else{
+					if(wireFrame.enabled && lineRenderer.enabled) wireFrame.SetWireFrame(0);
+					wireFrame.enabled=false;
+					lineRenderer.enabled=false;
+				}
 			}
 		}
-		
 	}
 	
 	public float GetMaxVision(){
 		return vision;	
 	}
+	
+//	private Vector3 center;
+	private float GetLeastDistance(Collider[] colliders){
+		float distance=0;
+		int counter=0;
+		foreach(Collider col in colliders){
+			if(col.GetComponent<WireFrame>().HasParent()){
+				if(counter==0){
+					distance=Vector3.Distance(c_transform.position, col.ClosestPointOnBounds(c_transform.position));
+					counter=1;
+				}else{
+					if(distance>=Vector3.Distance(c_transform.position, col.ClosestPointOnBounds(c_transform.position))){
+						distance=Vector3.Distance(c_transform.position, col.ClosestPointOnBounds(c_transform.position));	
+					}
+				}
+			}
+		}
+		return distance;
+	}
+	
 }
--- Game/Assets/_Raf/Scripts/WireFrameParent.cs
+++ Game/Assets/_Raf/Scripts/WireFrameParent.cs
@@ -2,22 +2,38 @@ using UnityEngine;
 using System.Collections;
 
 public class WireFrameParent : MonoBehaviour {
-	WireFrame[] wireFrame;
+	private Transform[] tr;
+	private WireFrame wf;
+	private LineRenderer lr;
+	private Vector3 tempVector3;
+	
+	public float roughness = 0;
+	public float thickness = 0;
+	private float maxDistance=0;
 	void Start () {
-		wireFrame=GetComponentsInChildren<WireFrame>();
-		foreach(WireFrame wf in wireFrame){
-			wf.SetHasParent(true);
+		tr=transform.GetComponentsInChildren<Transform>();
+		for(int i=1 ; i<tr.Length ; i++) tr[i].GetComponent<WireFrame>().SetParent(this);
+		maxDistance=GameObject.FindGameObjectWithTag("Player").GetComponent<DetectNearObjects>().GetMaxVision();
+		tempVector3=Vector3.one;
+	}
+	public void SetWireFrames(float distance){
+		for(int i=1 ; i<tr.Length ; i++){
+			wf=tr[i].GetComponent<WireFrame>();
+			lr=tr[i].GetComponent<LineRenderer>();
+			
+			if(distance!=0){
+				if(!wf.enabled) wf.enabled=true;
+				if(!lr.enabled) lr.enabled=true;
+			}else{
+				if(wf.enabled) wf.enabled=false;
+				if(lr.enabled) lr.enabled=false;
+			}
+			if(wf.enabled && lr.enabled){
+				tempVector3.x=((maxDistance-distance)/maxDistance)*thickness;
+				lr.SetWidth(tempVector3.x, tempVector3.x);
+				tempVector3.x=roughness*Random.value*3;
+				lr.sharedMaterial.mainTextureScale = tempVector3;
+			}
 		}
 	}
-	
-	/*
-	public void SetWireFrame(float distance){
-		if(c_lineRenderer){
-			tempVector3.x=((maxDistance-distance)/maxDistance)*thickness;
-			c_lineRenderer.SetWidth(tempVector3.x, tempVector3.x);
-			tempVector3.x=r*Random.value*3;
-			c_lineRenderer.sharedMaterial.mainTextureScale = tempVector3;
-		}
-	}*/
-	
 }
